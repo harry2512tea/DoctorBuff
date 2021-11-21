75259b2b369abcd77b2f08a9980038c09b2a00fa
harry2512tea <benjamin.t.harrison24@gmail.com>
2021-11-21 21:34:46 +0000

    rearranged the code to be easier to read

---
+++ DoctorBuff/Commands.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandSystem;
+using Exiled.API.Features;
+using MEC;
+using UnityEngine;
+
+namespace DoctorBuff
+{
+    [CommandHandler(typeof(ClientCommandHandler))]
+    class reinforcements : ICommand
+    {
+        //private readonly DoctorBuff doctorBuff;
+        //public reinforcements(DoctorBuff doctorBuff) => this.doctorBuff = doctorBuff;
+        public string Command { get; } = "CallRefincorcements";
+
+        public string[] Aliases { get; } = { "cr" };
+
+        public string Description { get; } = "Summons a zombie from the spectators";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            Player player = Player.Get((RemoteAdmin.PlayerCommandSender)sender);
+            if (player.Role == RoleType.Scp049)
+            {
+                List<Player> list = Player.List.Where(r => r.Role == RoleType.Spectator).ToList();
+
+                if (SCP049AbilityControl.CureCounter < DoctorBuff.config.MinCures)
+                {
+                    response = $"Not enough revives";
+                    return true;
+                }
+
+                if (SCP049AbilityControl.Cooldown > 0)
+                {
+                    response = $"Ability is still on cooldown. Time remaining: { SCP049AbilityControl.Cooldown }";
+                    return true;
+                }
+
+                if (list.IsEmpty())
+                {
+                    response = "No Spectators to spawn as Zombies";
+                    return true;
+                }
+
+                var index = 0;
+                index += new System.Random().Next(list.Count);
+                var selected = list[index];
+
+                selected.SetRole(RoleType.Scp0492);
+                selected.Health = selected.MaxHealth;
+
+                Timing.CallDelayed(0.5f, () =>
+                {
+                    selected.Position = new Vector3(player.Position.x, player.Position.y, player.Position.z);
+                });
+
+                index = 0;
+
+                SCP049AbilityControl.Cooldown = DoctorBuff.config.Cooldown;
+                Timing.RunCoroutine(SCP049AbilityControl.StartCooldownTimer(), "SCP049_Active_Cooldown");
+                response = "summoning Zombie";
+                return true;
+            }
+            else
+            {
+                response = "you have to be 049 to summon zombies";
+                return false;
+            }
+        }
+    }
+}
--- DoctorBuff/SCP049AbilityControl.cs
+++ DoctorBuff/SCP049AbilityControl.cs
@@ -9,7 +9,6 @@ using Exiled.Events.EventArgs;
 using Hints;
 using MEC;
 using UnityEngine;
-using CommandSystem;
 using Player = Exiled.API.Features.Player;
 
 namespace DoctorBuff
@@ -116,66 +115,5 @@ namespace DoctorBuff
             Timing.KillCoroutines("SCP049_Active_Cooldown");
         }
     }
-    [CommandHandler(typeof(ClientCommandHandler))]
-    class reinforcements : ICommand
-    {
-        //private readonly DoctorBuff doctorBuff;
-        //public reinforcements(DoctorBuff doctorBuff) => this.doctorBuff = doctorBuff;
-        public string Command { get; } = "CallRefincorcements";
-
-        public string[] Aliases { get; } = { "cr" };
-
-        public string Description { get; } = "Summons a zombie from the spectators";
-
-        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
-        {
-            Player player = Player.Get((RemoteAdmin.PlayerCommandSender)sender);
-            if (player.Role == RoleType.Scp049)
-            {
-                List<Player> list = Player.List.Where(r => r.Role == RoleType.Spectator).ToList();
-
-                if (SCP049AbilityControl.CureCounter < DoctorBuff.config.MinCures)
-                {
-                    response = $"Not enough revives";
-                    return true;
-                }
-
-                if (SCP049AbilityControl.Cooldown > 0)
-                {
-                    response = $"Ability is still on cooldown. Time remaining: { SCP049AbilityControl.Cooldown }";
-                    return true;
-                }
-
-                if (list.IsEmpty())
-                {
-                    response = "No Spectators to spawn as Zombies";
-                    return true;
-                }
-
-                var index = 0;
-                index += new System.Random().Next(list.Count);
-                var selected = list[index];
-
-                selected.SetRole(RoleType.Scp0492);
-                selected.Health = selected.MaxHealth;
-
-                Timing.CallDelayed(0.5f, () =>
-                {
-                    selected.Position = new Vector3(player.Position.x, player.Position.y, player.Position.z);
-                });
-
-                index = 0;
-
-                SCP049AbilityControl.Cooldown = DoctorBuff.config.Cooldown;
-                Timing.RunCoroutine(SCP049AbilityControl.StartCooldownTimer(), "SCP049_Active_Cooldown");
-                response = "summoning Zombie";
-                return true;
-            }
-            else
-            {
-                response = "you have to be 049 to summon zombies";
-                return false;
-            }
-        }
-    }
+    
 }
